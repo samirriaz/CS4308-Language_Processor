9acd5a97514f186b491e96d9e8d4c9b942e15f63
samirriaz <42588325+samirriaz@users.noreply.github.com>
2020-10-05 16:56:55 -0400

    Updated Scanner.java to match what Thuan uploaded
    I added a "main" method which hardcodes some Ada code, saves it as a string, and calls the getOperand method with it.

--- Scanner.java
+++ Scanner.java
@@ -1,7 +1,54 @@
-public class Scanner
-// global variables
+import java.util.*
+import java.util.LinkedList;
 
-// Scanner constructor
-public Scanner () {
+public class Scanner {
 
+  // operations to test
+  private static enum Type {
+    add, sub, mult, div, remain, op
+      // CONSIDER: removing REMAIN ... to make it different and easier
+  }
+  
+  // Token Class: holds value/type of token
+  private static class Token<T_Val, T_Type> {
+    private final T_Val value;
+    private final T_Type type;
+    
+  public Scanner(T_Val value, T_Type type) {
+    this.type = type;
+    this.value = value;
+  }
+    
+  // toString Override
+  @Override
+  public String toString() {
+    return "Token Value: " + this.value + " ... Token Type: " + this.type;
+  }
+}
+
+// getOperand: takes in a string and index and returns operand at that index
+private static String getOperand(String op, int index) {
+  for (int i=index; i<op.length(); {
+    if (Character.isLetterOrDigit(op.charAt(i))) {
+      i++;
+    }
+    else {
+    return op.substring(index, i);
+  }
+  return op.substring(index, i)
+}
+  public static void main(String[] args) {
+    // Save the Ada code as a String
+    // Use getOperand to parse through this String & print tokens
+    String ADA_CODE =
+      "procedure Main is" +
+      "begin" +
+      "a : Integer := 2;" +
+      "b : Integer := 3;" +
+      "s : String := \"The sum of \";" +
+      "Put_Line (s & a & \" and \" & b & \" is \" & (a+b) );" +
+      "end";
+    getOperand(ADA_CODE, 0);
+
+  }
 }
