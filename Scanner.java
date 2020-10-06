c5d21359190b9387889878529bc03a6da8d8d288
samirriaz <42588325+samirriaz@users.noreply.github.com>
2020-10-06 16:54:33 -0400

    Completely changed Scanner.java
    I wrote this code from scratch since I couldn't understand the code that was originally submitted here.

--- Scanner.java
+++ Scanner.java
@@ -1,59 +1,96 @@
-  
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ListIterator;
 import java.util.LinkedList;
 
-public class scanner {
+public class LexicalScanner {
+    public static int prompt_user(LinkedList tokens) {
+        // lineNumbers: holds each individual token's line number
+        LinkedList lineNumbers = new LinkedList();
 
-  // operations to test
-  private static enum Type {
-    add, sub, mult, div, remain, op
-      // CONSIDER: removing REMAIN ... to make it different and easier
-  }
-  
-  // Token Class: holds value/type of token
-  private static class Token<T_Val, T_Type> {
-    private final T_Val value;
-    private final T_Type type;
-    
-  public Token(T_Val value, T_Type type) {
-    this.type = type;
-    this.value = value;
-  }
-    
-  // toString Override
-  @Override
-  public String toString() {
-    return "Token Value: " + this.value + " ... Token Type: " + this.type;
-  }
-}
+        int lines=1;
 
-// getOperand: takes in a string and index and returns operand at that index
-public static String getOperand(String op, int index) {
-  for (int i=index; i<op.length();) {
-    if (Character.isLetterOrDigit(op.charAt(i))) {
-      i++;
-         }
-    else {
-    return op.substring(index, i);
-         }
-  return op.substring(index, i);
+        Scanner scan = new java.util.Scanner(System.in);
+        System.out.println("Enter Ada Code line-by-line inserting a space between ALL tokens.\nEnter 0 when finished.");
+        //                                               ^This^ breaks the WHILE loop
+        // Each WHILE iteration is 1 line of Ada code entered
+        while (true) {
+            String line = scan.nextLine();
+            if (line.equals("0")) {
+                break;
+            }
+            // currentLine: create new temporary array on each iteration to store the contents
+            // of the current line of Ada code. Split by each [SPACE] character.
+            String[] currentLine = line.split(" ");
+            // FOR the length of the current line
+            //      ...add each token to grand List 'tokens'
+            //      ...update List 'lineNumbers' by adding the current line# * # of tokens on the line
+            for (int i=0; i< currentLine.length; i++) {
+                tokens.add(currentLine[i]);
+                lineNumbers.add((char)(lines));
+            }
+            // End of current WHILE iteration (user presses [ENTER] )
+            // 'lines' (counts # of Ada code lines) is incremented
+            ++lines;
+        }
+        return lines;
     }
-  }
-}
-class Test extends scanner{
-  public static void main(String[] args) {
-    // Save the Ada code as a String
-    // Use getOperand to parse through this String & print tokens
-    scanner scan = new scanner();
-    String ADA_CODE =
-      "procedure Main is" +
-      "begin" +
-      "a : Integer := 2;" +
-      "b : Integer := 3;" +
-      "s : String := \"The sum of \";" +
-      "Put_Line (s & a & \" and \" & b & \" is \" & (a+b) );" +
-      "end";
-    scan.getOperand(ADA_CODE, 0);
+    // 'category' is passed each token individually
+    // each token is cross-referenced with the provided arrays of different lexeme categories
+    //          CONSIDER REWORKING...could drastically slow down execution.
+    public String category(String token) {
+        String token_lex = "NonLexeme", token_id="VAR_ID";
+        LinkedList ada_keywords = new LinkedList(Arrays.asList("abort","abs","abstract","accept","access", "aliased","all","and","array","at",
+                 "begin","body","case","constant","declare","delay","delta","digits","do",
+                 "else","elsif","end","entry","exception","exit","for","function","generic","goto",
+                 "if","in","interface","is","limited","loop","mod","new","not","null","of","or",
+                 "others","out","overriding","package","pragma","private","procedure","protected",
+                 "raise","range","record","rem","rename","requeue","return","reverse","select",
+                 "separate","some","subtype","synchronized","tagged","task","terminate","then","type",
+                 "until","use","while","with","xor"));
+        ListIterator iterate_keywords = ada_keywords.listIterator();
+        while (iterate_keywords.hasNext()) {
+            if (iterate_keywords.next().equals(token)) {
+                token_lex = "Lexeme";
+            }
+        }
 
-  }
+        LinkedList operators = new LinkedList(Arrays.asList( "+","-","*","/","="));
+        ListIterator iterate_ops = operators.listIterator();
+        while (iterate_ops.hasNext()) {
+            if (iterate_ops.next().equals(token)) {
+                switch (token) {
+                    case "+":
+                        token_id="PLUS";
+                    case "-":
+                        token_id="MINUS";
+                    case "*":
+                        token_id="TIMES";
+                    case "/":
+                        token_id="DIVIDE";
+                    case "=":
+                        token_id="ASSIGN";
+                }
+            }
+        }
+        return "" + token_lex + "..." + token_id;
+    }
+    public static void main(String[] args) {
+        LinkedList tokens = new LinkedList();
+        int lines = prompt_user(tokens);
+        for (int i=0; i<tokens.size(); i++) {
+            System.out.print("Tokens[" + i + "]: ");
+            System.out.println(tokens.get(i));
+        }
+        System.out.println("Number of lines: " + (lines-1) +"\n\n");
+    }
 }
+
+
+/*
+User enters line.
+Split the line, save each word into array
+Use array to update main output LinkedList
+
+
+ */
